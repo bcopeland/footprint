51d9840a90b9d4fe99518e2f70ad23b058ee173f
Bob Copeland <me@bobcopeland.com>
2013-06-07 22:52:04 -0400

    Some code size reductions

--- decrypt.c
+++ decrypt.c
@@ -3,50 +3,48 @@
 
 char str[] = "43(:7&!#3&@>$%^|]:&6;<7*-$}9{;!*!$5{<;-=^8]#:5<@#%#&!5!@40207#9($3&)7<$1";
 
-int b32dec(char *in, char *out, int len)
+int b(char *in, char *o, int len)
 {
-    int i;
-    char alpha[] = "3:5[9&2^]{7}*<-8@=4(6#!>|)0+;1$%";
+    int i,j,z;
+    char a[] = "3:5[9&2^]{7}*<-8@=4(6#!>|)0+;1$%";
     for (i=0; i < len; i++)
     {
-        unsigned char bits = strchr(alpha, in[i])-alpha;
-        int j = (i / 8) * 5;
-        switch (i % 8) {
-            case 0:
-                out[j + 0] |= bits << 3;
-                break;
-            case 1:
-                out[j + 0] |= bits >> 2;
-                out[j + 1] |= bits << 6;
-                break;
-            case 2:
-                out[j + 1] |= bits << 1;
-                break;
-            case 3:
-                out[j + 1] |= bits >> 4;
-                out[j + 2] |= bits << 4;
-                break;
-            case 4:
-                out[j + 2] |= bits >> 1;
-                out[j + 3] |= bits << 7;
-                break;
-            case 5:
-                out[j + 3] |= bits << 2;
-                break;
-            case 6:
-                out[j + 3] |= bits >> 3;
-                out[j + 4] |= bits << 5;
-                break;
-            case 7:
-                out[j + 4] |= bits;
-                break;
+        unsigned char k = strchr(a, in[i])-a;
+        j = (i / 8) * 5;
+        z = i & 7;
+        if (!z)
+                o[j] |= k << 3;
+        if (z==1) {
+                o[j] |= k >> 2;
+                o[j + z] |= k << 6;
+        }
+        if (z==2) {
+                o[j + 1] |= k << 1;
+        }
+        if (z==3) {
+                o[j + 1] |= k >> 4;
+                o[j + 2] |= k << 4;
+        }
+        if (z==4) {
+                o[j + 2] |= k >> 1;
+                o[j + 3] |= k << 7;
+        }
+        if (z==5) {
+                o[j + 3] |= k << 2;
+        }
+        if (z==6) {
+                o[j + 3] |= k >> 3;
+                o[j + 4] |= k << 5;
+        }
+        if (z==7) {
+                o[j + 4] |= k;
         }
     }
     return i/8 * 5;
 }
 
 
-int keysched(unsigned char *x, unsigned char *key, int keylen)
+int k(unsigned char *x, unsigned char *key, int l)
 {
     int i;
     unsigned char tmp, a = 0;
@@ -56,28 +54,26 @@ int keysched(unsigned char *x, unsigned char *key, int keylen)
 
     for (i=0; i < 256; i++)
     {
-        a += x[i] + key[i % keylen];
+        a += x[i] + key[i % l];
         tmp = x[i];
         x[i] = x[a];
         x[a] = tmp;
     }
 }
 
-int crypt(unsigned char *x, unsigned char *y, int len)
+int c(unsigned char *x, unsigned char *y, int l)
 {
-    unsigned char a;
     unsigned char b = 0;
     unsigned char tmp;
     int i;
 
-    for (i=0; i < len; i++)
+    for (i=1; i <= l; i++)
     {
-        a = i+1;
-        b += x[a];
-        tmp = x[a];
-        x[a] = x[b];
+        b += x[i];
+        tmp = x[i];
+        x[i] = x[b];
         x[b] = tmp;
-        y[i] ^= x[(x[a] + x[b]) & 0xff];
+        y[i-1] ^= x[(x[i] + x[b]) & 0xff];
     }
 }
 
@@ -88,8 +84,8 @@ int main()
     unsigned char crypt_text[sizeof(str)] = {};
     int len;
 
-    len = b32dec(str, crypt_text, strlen(str));
-    keysched(x, key, strlen(key));
-    crypt(x, crypt_text, len);
+    len = b(str, crypt_text, strlen(str));
+    k(x, key, strlen(key));
+    c(x, crypt_text, len);
     printf("%s\n", crypt_text);
 }
